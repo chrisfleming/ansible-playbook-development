1511ff6f4fa2402d46abeafdd09cbcc272ea6124
Chris Fleming <chflemin@ns.lab27.illuminate.solutions>
2019-02-27 17:43:42 +0000

    Improved filtering of email, mimimal html template, premailer on output

--- roles/email/files/filter_email.py
+++ roles/email/files/filter_email.py
@@ -25,8 +25,79 @@ html = re.sub('mso-bookmark:_[A-Za-z0-9]*', '', html)
 
 soup = BeautifulSoup(html, features="lxml")
 
+
+def any_in(x, y):
+
+    for a in x:
+        if a in y:
+            #print("%s in %s" % (a, x))
+            return True
+
+    return False
+
+
+
 for match in soup.find_all(text=re.compile('^\xa0$')):
     match.parent.unwrap()
+
+# Remove all span/style information except for colour if not black.
+
+for match in soup.find_all('span', {"style" : True}):
+    remaining_style = []
+    is_code = False
+    match['style'].replace("&quot;", '"')
+    if not len(match['style']):
+        match.unwrap()
+        continue
+
+    for style in match['style'].split(';'):
+        #print(style)
+        #try:
+        key, value = style.split(':')
+        # except:
+        #    key = ""
+        #    value = ""
+        #    print("  BROKEN: %s" % (match))
+
+        if key == "font-family":
+            fonts = value.split(',')
+            fonts = [x.lower() for x in fonts]
+            fonts = [x.replace('"', '') for x in fonts]
+            fonts = [x.strip() for x in fonts]
+            #print("    %s" % fonts)
+
+            fixed_fonts = ["courier new", "consolas"]
+
+            if any_in(fixed_fonts, fonts):
+                #print("HAVE CODE")
+                is_code = True
+
+        if key == "color":
+            if value not in ['black']:
+                remaining_style.append(style)
+
+    if is_code:
+        # TODO smarter about this
+        # eg simpliy <o:p></o:p>
+        #print("Have code")
+        #print(match)
+        match.wrap(soup.new_tag("pre"))
+        match.unwrap()
+        continue
+
+    match['style'] = ";".join(remaining_style)
+
+
+
+
+
+
+
+            
+
+
+
+
 
 for match in soup.find_all('span', attrs={"style": "mso-fareast-language:EN-US"}):
     match.unwrap()
